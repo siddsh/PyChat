3cf6252f6f8b555726eb9375e633a1dfccac0713
Siddhant Kulkarni <siddsh>
2020-08-09 03:19:11 +0530

    Working

---
+++ client.py
@@ -0,0 +1,28 @@
+import threading
+import socket
+nickname = input("Please input your nickname: ")
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 59696))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print('An Error Occured')
+            client.close()
+            break
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode('ascii'))
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
